03521445a0f09a7122b332b670f2bc3be1f21040
Dev.Pachon <dahp-dahp@hotmail.com>
2020-05-14 13:04:41 -0500

    commit of the sadness :c

--- src/ui/AlgorithmsRaceGUI.java
+++ src/ui/AlgorithmsRaceGUI.java
@@ -1,8 +1,17 @@
 package ui;
 
+import java.time.LocalTime;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -10,8 +19,59 @@ public class AlgorithmsRaceGUI {
 	
 	private Stage stage;
 	
-	
-	
+	@FXML
+    private Label arrayListCronometer;
+
+    @FXML
+    private Label CronometerLabel;
+
+    @FXML
+    private Label linkedListCronometer;
+
+    @FXML
+    private Label binarySearchTreeCronometer;
+
+    @FXML
+    private TextField NumberTextField;
+
+    @FXML
+    private Button runButton;
+
+    @FXML
+    private RadioButton addRadioBtn;
+
+    @FXML
+    private ToggleGroup algorithmGroup;
+
+    @FXML
+    private RadioButton consultRadioBtn;
+
+    @FXML
+    private RadioButton deleteRadioBtn;
+
+    @FXML
+    private RadioButton iterativeRadioBtn;
+
+    @FXML
+    private ToggleGroup modeGroup;
+
+    @FXML
+    private RadioButton recursiveRadioBtn;
+
+    @FXML
+    private Circle circle1;
+
+    @FXML
+    private Circle circle2;
+
+    @FXML
+    private ProgressBar progressBarAL;
+
+    @FXML
+    private ProgressBar progressBarLL;
+
+    @FXML
+    private ProgressBar progressBarBST;
 	
 	public AlgorithmsRaceGUI(Stage st) {
 		stage = st;
@@ -36,7 +96,8 @@ public class AlgorithmsRaceGUI {
 	}
 	
 	public void updateMainCronometer(long l) {
-		
+		LocalTime lt = new LocalTime(l,);
+		CronometerLabel.setText();
 	}
 	
 	public void updateALCronometer(long l) {
